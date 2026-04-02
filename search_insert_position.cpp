08db7146c5bf97dae8c216a147a67280ca8a5bb7
Abhishek Singh <abhitil654@gmail.com>
2026-04-02 22:09:08 +0530

    maximum_subarray

--- search_insert_position.cpp
+++ search_insert_position.cpp
@@ -7,16 +7,44 @@ class Solution
 public:
     // time complexity: O(n)
     // space complexity: O(1)
+    // int searchInsert(vector<int> &nums, int target)
+    // {
+    //     for (int i = 0; i < nums.size(); i++)
+    //     {
+    //         if (nums[i] >= target)
+    //         {
+    //             return i;
+    //         }
+    //     }
+    //     return nums.size();
+    // }
+
+    // Optimal Approach: Binary Search
+    // Time Complexity: O(log n)
+    // Space Complexity: O(1)
     int searchInsert(vector<int> &nums, int target)
     {
-        for (int i = 0; i < nums.size(); i++)
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right)
         {
-            if (nums[i] >= target)
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
             {
-                return i;
+                return mid;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
             }
         }
-        return nums.size();
+
+        return left;
     }
 };
 
